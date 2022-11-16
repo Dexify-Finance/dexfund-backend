20b8114316e4268af5d6741104436dc67ed6ec70
S.Tanaka <stenor0515@gmail.com>
2022-11-16 12:35:51 -0700

    changed package.json

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('dexfund')
     .build();
+
   app.setGlobalPrefix('api', { exclude: ['healthcheck'] });
 
   const document = SwaggerModule.createDocument(app, config);
