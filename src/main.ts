9ef29252ce5eefa312e681b0335adc2c885edef2
S.Tanaka <stenor0515@gmail.com>
2022-11-16 09:40:05 -0700

    added swagger api documentation and \server status check api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Dexfund Backend')
+    .setDescription('The dexfund API description')
+    .setVersion('1.0')
+    .addTag('dexfund')
+    .build();
+
+  app.setGlobalPrefix('api', { exclude: ['metrics', 'healthcheck'] });
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
